0ce47c61eee01f9bf82f799c292b1f870a6e8efd
Affid <andrei.fedorov000@gmail.com>
2025-06-19 18:15:25 +0200

    Revert "Add testing endpoint"
    This reverts commit bf406894e54c2c2a93d3a55ca3e5f1bc8945d488.

--- src/main/kotlin/org/affidtech/dnd/admin/web/controller/FileController.kt
+++ src/main/kotlin/org/affidtech/dnd/admin/web/controller/FileController.kt
@@ -1,15 +1,14 @@
 package org.affidtech.dnd.admin.web.controller
 
 import jakarta.servlet.http.HttpServletResponse
-import org.affidtech.dnd.admin.config.ConfigPrinter
 import org.affidtech.dnd.admin.service.file.FileStorageService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
+import java.util.UUID
 
 @RestController
-class FileController(val storage: FileStorageService, val configPrinter: ConfigPrinter) {
+class FileController(val storage: FileStorageService) {
 	@GetMapping("/files/adventure-covers/{id}")
 	fun getCover(@PathVariable id: UUID, response: HttpServletResponse) {
 		val resource = storage.loadCover(id)
@@ -17,10 +16,5 @@ class FileController(val storage: FileStorageService, val configPrinter: ConfigP
 		resource.inputStream.use { input ->
 			input.copyTo(response.outputStream)
 		}
-	}
-	
-	@GetMapping("/files/test")
-	fun testConfig() {
-		configPrinter.printConfig()
 	}
 }
